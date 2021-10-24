a2a1bf6af97a2dcb79ff10f60cc319e7b5d62f09
Mix <mnixry@users.noreply.github.com>
2021-10-25 00:06:53 +0800

    :art: 添加多个Pixiv API调用函数

--- src/plugin-hibi/pixiv/network.ts
+++ src/plugin-hibi/pixiv/network.ts
@@ -1,10 +1,19 @@
-import { PixivRankType, PixivRankData, PixivError } from "./models";
+import {
+  PixivRankType,
+  PixivRankData,
+  PixivError,
+  PixivIllustData,
+  PixivMemberData,
+  PixivIllustType,
+  PixivMemberIllustData,
+} from "./models";
 import { hibi } from "../network";
 
 export function proxyPixivImage(source: string): string {
   const url = new URL(source);
   url.host = "pximg.obfs.dev";
-  return url.href;
+  url.protocol = "https";
+  return url.toString();
 }
 
 export async function rank(options: {
@@ -19,8 +28,41 @@ export async function rank(options: {
   return response.data;
 }
 
-export async function illust(id: number) {
-  const response = await hibi.get<PixivRankData | PixivError>(`pixiv/illust`, {
-    params: { id },
-  });
+export async function illust(id: number): Promise<PixivIllustData> {
+  const response = await hibi.get<PixivIllustData | PixivError>(
+    `pixiv/illust`,
+    {
+      params: { id },
+    }
+  );
+  if ("error" in response.data)
+    throw new Error(response.data.error.user_message);
+  return response.data;
+}
+
+export async function member(id: number): Promise<PixivMemberData> {
+  const response = await hibi.get<PixivMemberData | PixivError>(
+    `pixiv/member`,
+    {
+      params: { id },
+    }
+  );
+  if ("error" in response.data)
+    throw new Error(response.data.error.user_message);
+  return response.data;
+}
+
+export async function memberIllust(
+  id: number,
+  options: { type?: keyof typeof PixivIllustType; page?: number }
+): Promise<PixivMemberIllustData> {
+  const response = await hibi.get<PixivMemberIllustData | PixivError>(
+    `pixiv/member_illust`,
+    {
+      params: { ...options, id },
+    }
+  );
+  if ("error" in response.data)
+    throw new Error(response.data.error.user_message);
+  return response.data;
 }
