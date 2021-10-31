ef925e038763da078d99149e00a023329b170a12
Mix <mnixry@users.noreply.github.com>
2021-10-31 17:31:03 +0800

    :loud_sound: :art: 优化网络请求日志记录方法

--- src/plugin-hibi/index.ts
+++ src/plugin-hibi/index.ts
@@ -1,7 +1,8 @@
 import { Context } from "koishi";
+import * as bilibili from "./bilibili";
 import * as network from "./network";
 import * as pixiv from "./pixiv";
 
 export function apply(ctx: Context) {
-  ctx.plugin(network).plugin(pixiv);
+  ctx.plugin(network).plugin(pixiv).plugin(bilibili);
 }
--- src/plugin-hibi/network.ts
+++ src/plugin-hibi/network.ts
@@ -5,36 +5,20 @@ import { Context } from "koishi";
 export const hibi = axios.create({
   baseURL: "https://api.obfs.dev/api/",
   headers: { "User-Agent": "TsubasaBot (koishi-plugin-hibi)" },
+  validateStatus: (status) => status < 400,
 });
 
 export function apply(ctx: Context) {
   const logger = ctx.logger("hibi.network");
+  const commonConfig = { data: false, prefixText: false },
+    errorConfig = { ...commonConfig, logger: logger.error },
+    debugConfig = { ...commonConfig, logger: logger.debug };
   hibi.interceptors.request.use(
-    (request) =>
-      AxiosLogger.requestLogger(request, {
-        logger: logger.debug.bind(this),
-        data: false,
-        prefixText: false,
-      }),
-    (error) =>
-      AxiosLogger.errorLogger(error, {
-        logger: logger.error.bind(this),
-        data: false,
-        prefixText: false,
-      }),
+    (request) => AxiosLogger.requestLogger(request, debugConfig),
+    (error) => AxiosLogger.errorLogger(error, errorConfig),
   );
   hibi.interceptors.response.use(
-    (response) =>
-      AxiosLogger.responseLogger(response, {
-        logger: logger.debug.bind(this),
-        data: false,
-        prefixText: false,
-      }),
-    (error) =>
-      AxiosLogger.errorLogger(error, {
-        logger: logger.error.bind(this),
-        data: false,
-        prefixText: false,
-      }),
+    (response) => AxiosLogger.responseLogger(response, debugConfig),
+    (error) => AxiosLogger.errorLogger(error, errorConfig),
   );
 }
