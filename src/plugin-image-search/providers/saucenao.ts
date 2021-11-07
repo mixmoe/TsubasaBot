581223aedf811a0f273cae9ab86b01d8da332ef6
Mix <mnixry@users.noreply.github.com>
2021-11-07 17:57:23 +0800

    :construction: 添加`saucenao`图片搜索提供

---
+++ src/plugin-image-search/providers/saucenao.ts
@@ -0,0 +1,46 @@
+import * as cheerio from "cheerio";
+import * as FormData from "form-data";
+import * as _ from "lodash";
+import { request } from "../network";
+import { downloadImage } from "../utils";
+
+const BASE_URL = "https://saucenao.com";
+
+export function parse(body: string) {
+  const $ = cheerio.load(body, { decodeEntities: true });
+  return _.map($(".result"), (result) => {
+    const image = $(".resultimage img", result),
+      title = $(".resulttitle", result),
+      similarity = $(".resultsimilarityinfo", result),
+      misc = $(".resultmiscinfo > a", result),
+      content = $(".resultcontentcolumn", result);
+    const hiddenImage = image.attr("img-src2"),
+      imageUrl = hiddenImage ? hiddenImage : image.attr("src");
+    return {
+      image: new URL(<string>imageUrl, BASE_URL).toString(),
+      hidden: !!hiddenImage,
+      title: title.text(),
+      similarity: parseFloat(similarity.text()),
+      misc: _.map(misc, (m) => m.attribs.href),
+      content: _.map(
+        content,
+        (c) => $(c).text() + (c.tagName === "a" ? "" + c.attribs.href : ""),
+      ).join(`\n`),
+    };
+  })
+    .sort((a, b) => a.similarity - b.similarity)
+    .reverse();
+}
+
+export async function search(url: string, options: { hide?: boolean }) {
+  const form = new FormData(),
+    image = downloadImage(url);
+  form.append("file", image);
+  if (options.hide) form.append("hide", "3");
+  const { data } = await request.post<string>("/search.php", form, {
+    headers: form.getHeaders(request.defaults.headers),
+    baseURL: BASE_URL,
+    responseType: "text",
+  });
+  return parse(data);
+}
