bc3d374a61e096d5ee83797c251530dd3a52c16d
Mix <mnixry@users.noreply.github.com>
2021-11-09 00:59:59 +0800

    :sparkles: :art: 支持在模板中将任意URL转为segment

--- src/patch-template.ts
+++ src/patch-template.ts
@@ -1,17 +1,56 @@
-import { interpolate, template } from "@koishijs/utils";
+import { interpolate, segment, template } from "@koishijs/utils";
+import * as _ from "lodash";
 import * as mustache from "mustache";
 
 export type TemplateParam = { $mustache?: boolean; [key: string]: any };
+
+export class SegmentURL extends URL {
+  public static from(url: unknown) {
+    if (url instanceof URL) {
+      return new SegmentURL(url);
+    } else if (
+      typeof url === "string" &&
+      (url.startsWith("http:") || url.startsWith("https:"))
+    ) {
+      try {
+        return new SegmentURL(url);
+      } catch {
+        return;
+      }
+    }
+  }
+
+  get image() {
+    return segment.image(this.href);
+  }
+
+  get audio() {
+    return segment.audio(this.href);
+  }
+
+  get video() {
+    return segment.video(this.href);
+  }
+
+  get file() {
+    return segment.file(this.href);
+  }
+}
 
 export function format(source: string): string;
 export function format(source: string, params: TemplateParam): string;
 export function format(source: string, ...params): string {
   const [context] = params;
 
-  if (context && typeof context === "object")
-    source = (context as TemplateParam).$mustache
-      ? mustache.render(source, context)
-      : interpolate(source, context);
+  if (context && typeof context === "object") {
+    const { $mustache, ...param } = _.cloneDeepWith<TemplateParam>(
+      context,
+      SegmentURL.from,
+    );
+    source = $mustache
+      ? mustache.render(source, param, undefined, { escape: segment.escape })
+      : interpolate(source, param);
+  }
 
   let result = "",
     cap: RegExpExecArray | null;
