3e8e2c67e044229afb188f77babe007fb19b2489
Mix <mnixry@users.noreply.github.com>
2021-11-10 00:27:13 +0800

    :memo: 添加`ascii2d`搜索消息模板

--- src/plugin-image-search/utils.ts
+++ src/plugin-image-search/utils.ts
@@ -1,6 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
 import { request } from "./network";
 
 export async function downloadImage(url: string): Promise<Buffer> {
   const response = await request.get(url, { responseType: "arraybuffer" });
   return Buffer.from(<ArrayBuffer>response.data);
+}
+
+export function readTemplate(file: string, ext = ".mustache"): string {
+  const { root, dir, name } = path.parse(file);
+  return fs.readFileSync(path.format({ root, dir, name, ext }), "utf8");
+}
+
+export function removeUndefined<T>(value: T | undefined): value is T {
+  return value !== undefined;
 }
