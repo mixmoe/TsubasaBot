6369dfc92d5ee5dee3570de40587c345bba3ad12
Mix <mnixry@users.noreply.github.com>
2021-11-10 00:26:46 +0800

    :bug: 修复`ascii2d`搜图信息获取错误

--- src/plugin-image-search/providers/ascii2d.ts
+++ src/plugin-image-search/providers/ascii2d.ts
@@ -1,10 +1,11 @@
 import * as cheerio from "cheerio";
-import * as FormData from "form-data";
 import * as _ from "lodash";
 import { request } from "../network";
-import { downloadImage } from "../utils";
+import { readTemplate, removeUndefined } from "../utils";
 
 export const BASE_URL = "https://ascii2d.net/";
+
+export const TEMPLATE = readTemplate(__filename);
 
 export function parse(body: string) {
   const $ = cheerio.load(body, { decodeEntities: true });
@@ -14,27 +15,26 @@ export function parse(body: string) {
       info = $(".info-box > .text-muted", item),
       [image] = $(".image-box > img", item);
 
-    const [source, author] =
-      $(".external", detail).length > 0
-        ? $(":first-child > a[rel=noopener]", detail)
-        : $(".external > a[rel=noopener]", detail);
+    const [source, author] = $("a[rel=noopener]", detail);
+
+    if (!source && !author) return;
 
     return {
       hash: hash.text(),
       info: info.text(),
       image: new URL(image.attribs.src, BASE_URL).toString(),
-      source: { link: source?.attribs.href, text: $(source).text() },
-      author: { link: author?.attribs.href, text: $(author).text() },
+      source: source
+        ? { link: source.attribs.href, text: $(source).text() }
+        : undefined,
+      author: author
+        ? { link: author.attribs.href, text: $(author).text() }
+        : undefined,
     };
-  });
+  }).filter(removeUndefined);
 }
 
 export async function search(url: string, type: "color" | "bovw" = "color") {
-  const form = new FormData(),
-    image = await downloadImage(url);
-  form.append("file", image);
-  const { headers } = await request.post("/search/multi", form, {
-    headers: form.getHeaders(request.defaults.headers),
+  const { headers } = await request.get("/search/url/" + url, {
     baseURL: BASE_URL,
     maxRedirects: 0,
   });
