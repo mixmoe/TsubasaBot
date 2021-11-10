abae87fa57c9e4236649478381132ee78cf44359
Mix <mnixry@users.noreply.github.com>
2021-11-11 00:40:39 +0800

    :alembic: :sparkles: 验证`ehentai`以图搜图功能可用性

--- src/plugin-image-search/index.ts
+++ src/plugin-image-search/index.ts
@@ -1,8 +1,10 @@
 import { Context, segment, template } from "koishi";
 import * as network from "./network";
 import * as ascii2d from "./providers/ascii2d";
+import * as ehentai from "./providers/ehentai";
 
 template.set("ascii2d", ascii2d.TEMPLATE);
+template.set("ehentai", ehentai.TEMPLATE);
 
 export function apply(ctx: Context) {
   ctx.plugin(network);
@@ -14,6 +16,18 @@ export function apply(ctx: Context) {
       .find((value) => value.type === "image")?.data.url;
     const response = await ascii2d.search(imageUrl!);
     return template("ascii2d", {
+      results: response.slice(0, 3),
+      $mustache: true,
+    });
+  });
+  ctx.command("ehentai").action(async ({ session }) => {
+    await session?.send("来张图");
+    const imageMessage = await session?.prompt(30 * 1000);
+    const imageUrl = segment
+      .parse(imageMessage!)
+      .find((value) => value.type === "image")?.data.url;
+    const response = await ehentai.search(imageUrl!);
+    return template("ehentai", {
       results: response.slice(0, 3),
       $mustache: true,
     });
