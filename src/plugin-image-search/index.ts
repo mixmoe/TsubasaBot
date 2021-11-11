b9761f1e17858ae8d06b0f81e39ed2557debf51e
Mix <mnixry@users.noreply.github.com>
2021-11-11 23:50:05 +0800

    :alembic: :sparkles: 验证`saucenao`和`iqdb`以图搜图可用性

--- src/plugin-image-search/index.ts
+++ src/plugin-image-search/index.ts
@@ -2,9 +2,13 @@ import { Context, segment, template } from "koishi";
 import * as network from "./network";
 import * as ascii2d from "./providers/ascii2d";
 import * as ehentai from "./providers/ehentai";
+import * as iqdb from "./providers/iqdb";
+import * as saucenao from "./providers/saucenao";
 
 template.set("ascii2d", ascii2d.TEMPLATE);
 template.set("ehentai", ehentai.TEMPLATE);
+template.set("iqdb", iqdb.TEMPLATE);
+template.set("saucenao", saucenao.TEMPLATE);
 
 export function apply(ctx: Context) {
   ctx.plugin(network);
@@ -28,6 +32,30 @@ export function apply(ctx: Context) {
       .find((value) => value.type === "image")?.data.url;
     const response = await ehentai.search(imageUrl!);
     return template("ehentai", {
+      results: response.slice(0, 3),
+      $mustache: true,
+    });
+  });
+  ctx.command("iqdb").action(async ({ session }) => {
+    await session?.send("来张图");
+    const imageMessage = await session?.prompt(30 * 1000);
+    const imageUrl = segment
+      .parse(imageMessage!)
+      .find((value) => value.type === "image")?.data.url;
+    const response = await iqdb.search(imageUrl!);
+    return template("iqdb", {
+      results: response.slice(0, 3),
+      $mustache: true,
+    });
+  });
+  ctx.command("saucenao").action(async ({ session }) => {
+    await session?.send("来张图");
+    const imageMessage = await session?.prompt(30 * 1000);
+    const imageUrl = segment
+      .parse(imageMessage!)
+      .find((value) => value.type === "image")?.data.url;
+    const response = await saucenao.search(imageUrl!);
+    return template("saucenao", {
       results: response.slice(0, 3),
       $mustache: true,
     });
