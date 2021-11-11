4cb9380447f24025ac288874dc4e8e3869cfabc8
Mix <mnixry@users.noreply.github.com>
2021-11-12 00:48:37 +0800

    :alien: :art: 适配koishi更新, 添加对`go-cqhttp`直接启动的支持

--- app.ts
+++ app.ts
@@ -1,21 +1,34 @@
-import { App } from "koishi";
-import * as dotenv from "dotenv";
-import * as onebot from "@koishijs/plugin-adapter-onebot";
-import * as common from "@koishijs/plugin-common";
+import onebot from "@koishijs/plugin-adapter-onebot";
 import * as chat from "@koishijs/plugin-chat";
+import * as common from "@koishijs/plugin-common";
+import sqlite from "@koishijs/plugin-database-sqlite";
+import * as gocqhttp from "@koishijs/plugin-gocqhttp";
+import * as dotenv from "dotenv";
+import { App, Logger } from "koishi";
+import "./src/patch-segment";
+import "./src/patch-template";
 import * as hibi from "./src/plugin-hibi";
+import * as imageSearch from "./src/plugin-image-search";
 
 dotenv.config();
 
-const app = new App();
+if (process.env.LOG_LEVEL)
+  Logger.levels.base = Logger[process.env.LOG_LEVEL.toUpperCase()];
+
+const app = new App({ port: 3000, prettyErrors: true });
 
 app.plugin(onebot, {
-  protocol: "ws",
+  protocol: "ws-reverse",
   selfId: process.env.ONEBOT_SELF_ID,
   endpoint: process.env.ONEBOT_ENDPOINT,
+  password: process.env.GOCQHTTP_PASSWORD,
 });
+
+app.plugin(sqlite);
+app.plugin(gocqhttp);
 app.plugin(common);
 app.plugin(chat);
 app.plugin(hibi);
+app.plugin(imageSearch);
 
 app.start();
--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,13 @@
+import { Logger } from "koishi";
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV?: "development" | "production";
+      LOG_LEVEL?: Logger.Type;
       ONEBOT_SELF_ID?: string;
       ONEBOT_ENDPOINT?: string;
+      GOCQHTTP_PASSWORD?: string;
     }
   }
 }
