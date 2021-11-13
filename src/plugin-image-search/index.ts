0b4b6c769c7638237271eeb7763dc1d45a650887
Mix <mnixry@users.noreply.github.com>
2021-11-13 20:05:40 +0800

    :alembic: 验证`trace.moe`以图搜番可用性

--- src/plugin-image-search/index.ts
+++ src/plugin-image-search/index.ts
@@ -4,11 +4,13 @@ import * as ascii2d from "./providers/ascii2d";
 import * as ehentai from "./providers/ehentai";
 import * as iqdb from "./providers/iqdb";
 import * as saucenao from "./providers/saucenao";
+import * as tracemoe from "./providers/tracemoe";
 
 template.set("ascii2d", ascii2d.TEMPLATE);
 template.set("ehentai", ehentai.TEMPLATE);
 template.set("iqdb", iqdb.TEMPLATE);
 template.set("saucenao", saucenao.TEMPLATE);
+template.set("tracemoe", tracemoe.TEMPLATE);
 
 export function apply(ctx: Context) {
   ctx.plugin(network);
@@ -56,6 +58,18 @@ export function apply(ctx: Context) {
       .find((value) => value.type === "image")?.data.url;
     const response = await saucenao.search(imageUrl!);
     return template("saucenao", {
+      results: response.slice(0, 3),
+      $mustache: true,
+    });
+  });
+  ctx.command("tracemoe").action(async ({ session }) => {
+    await session?.send("来张图");
+    const imageMessage = await session?.prompt(30 * 1000);
+    const imageUrl = segment
+      .parse(imageMessage!)
+      .find((value) => value.type === "image")?.data.url;
+    const response = await tracemoe.search(imageUrl!);
+    return template("tracemoe", {
       results: response.slice(0, 3),
       $mustache: true,
     });
