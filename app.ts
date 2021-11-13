d7b7ef6cfe9980a222ea230c5610a198e9880138
Mix <mnixry@users.noreply.github.com>
2021-11-13 20:07:57 +0800

    :loud_sound: 优化调试日志对象显示长度

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import onebot from "@koishijs/plugin-adapter-onebot";
 import * as chat from "@koishijs/plugin-chat";
 import * as common from "@koishijs/plugin-common";
+import * as console from "@koishijs/plugin-console";
 import sqlite from "@koishijs/plugin-database-sqlite";
 import * as gocqhttp from "@koishijs/plugin-gocqhttp";
+import * as manager from "@koishijs/plugin-manager";
 import * as dotenv from "dotenv";
 import { App, Logger } from "koishi";
-import "./src/patch-segment";
+import { inspect } from "util";
 import "./src/patch-template";
 import * as hibi from "./src/plugin-hibi";
 import * as imageSearch from "./src/plugin-image-search";
@@ -15,8 +17,15 @@ dotenv.config();
 if (process.env.LOG_LEVEL)
   Logger.levels.base = Logger[process.env.LOG_LEVEL.toUpperCase()];
 
+Logger.formatters["o"] = (value, target?: Logger.Target) =>
+  inspect(value, { colors: !!target?.colors, maxStringLength: 256 }).replace(
+    /\s*\n\s*/g,
+    " ",
+  );
+
 const app = new App({ port: 3000, prettyErrors: true });
 
+app.plugin(gocqhttp);
 app.plugin(onebot, {
   protocol: "ws-reverse",
   selfId: process.env.ONEBOT_SELF_ID,
@@ -25,9 +34,11 @@ app.plugin(onebot, {
 });
 
 app.plugin(sqlite);
-app.plugin(gocqhttp);
-app.plugin(common);
+app.plugin(console);
+app.plugin(manager);
 app.plugin(chat);
+app.plugin(common);
+
 app.plugin(hibi);
 app.plugin(imageSearch);
 
