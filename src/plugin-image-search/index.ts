00c4d88a999b1bc2a7f0c43a34f030ec6fb3e2d1
Mix <mnixry@users.noreply.github.com>
2021-11-14 16:19:52 +0800

    :sparkles: 添加聚合搜图功能以取代实验性搜图功能

--- src/plugin-image-search/index.ts
+++ src/plugin-image-search/index.ts
@@ -1,77 +1,89 @@
-import { Context, segment, template } from "koishi";
+import { Context, template } from "koishi";
+import { ForwardMessageBuilder } from "../plugin-hibi/utils";
+import * as constants from "./constants";
 import * as network from "./network";
 import * as ascii2d from "./providers/ascii2d";
 import * as ehentai from "./providers/ehentai";
 import * as iqdb from "./providers/iqdb";
 import * as saucenao from "./providers/saucenao";
 import * as tracemoe from "./providers/tracemoe";
+import { firstImage, MultipleEnumerateType } from "./utils";
 
-template.set("ascii2d", ascii2d.TEMPLATE);
-template.set("ehentai", ehentai.TEMPLATE);
-template.set("iqdb", iqdb.TEMPLATE);
-template.set("saucenao", saucenao.TEMPLATE);
-template.set("tracemoe", tracemoe.TEMPLATE);
+export enum SupportedProviders {
+  ascii2d,
+  ehentai,
+  iqdb,
+  saucenao,
+  tracemoe,
+}
+
+export const providersMap = {
+  [SupportedProviders.ascii2d]: ascii2d.search,
+  [SupportedProviders.ehentai]: ehentai.search,
+  [SupportedProviders.iqdb]: iqdb.search,
+  [SupportedProviders.saucenao]: saucenao.search,
+  [SupportedProviders.tracemoe]: tracemoe.search,
+};
+
+export const templateMap = {
+  [SupportedProviders.ascii2d]: constants.TEMPLATE_ASCII2D,
+  [SupportedProviders.ehentai]: constants.TEMPLATE_EHENTAI,
+  [SupportedProviders.iqdb]: constants.TEMPLATE_IQDB,
+  [SupportedProviders.saucenao]: constants.TEMPLATE_SAUCENAO,
+  [SupportedProviders.tracemoe]: constants.TEMPLATE_TRACEMOE,
+};
+
+export function combine(ctx: Context) {
+  ctx
+    .command(`${constants.COMMAND_COMBINE}`, { minInterval: 30 * 1000 })
+    .alias("以图搜图", "聚合搜图")
+    .shortcut("以图搜番", { options: { engines: ["saucenao", "tracemoe"] } })
+    .option("engines", "-e <engines>", {
+      type: MultipleEnumerateType(SupportedProviders),
+    })
+    .option("limit", "-l <limit:posint>", { fallback: 3 })
+    .action(async ({ session, options }) => {
+      const engines =
+        options?.engines?.map((e) => SupportedProviders[e]) ??
+        Object.keys(providersMap).map<SupportedProviders>(Number);
+      let image = firstImage(session?.content ?? "");
+      if (!image) {
+        await session?.send(template(constants.TEMPLATE_ASK_IMAGE));
+        const promptResponse = await session?.prompt(30 * 1000).catch();
+        image = firstImage(promptResponse ?? "");
+      }
+      if (!image) return template(constants.TEMPLATE_BAD_INPUT);
+
+      const { username: name, userId: uin } = session!,
+        forward = new ForwardMessageBuilder(name, +uin!);
+
+      await session?.send(
+        template(
+          constants.TEMPLATE_START_PROMPT,
+          engines.map((e) => SupportedProviders[e]).join(),
+        ),
+      );
+
+      await Promise.all(
+        engines.map(async (engine: number) => {
+          const provider = providersMap[engine],
+            templateName = templateMap[engine];
+          const results = await provider(image).catch(() => undefined);
+          if (!results || results.length <= 0) return;
+          forward.add(
+            ...results
+              .slice(undefined, options?.limit)
+              .map((result) =>
+                template(templateName, { results: result, $mustache: true }),
+              ),
+          );
+        }),
+      );
+
+      await forward.send();
+    });
+}
 
 export function apply(ctx: Context) {
-  ctx.plugin(network);
-  ctx.command("ascii2d").action(async ({ session }) => {
-    await session?.send("来张图");
-    const imageMessage = await session?.prompt(30 * 1000);
-    const imageUrl = segment
-      .parse(imageMessage!)
-      .find((value) => value.type === "image")?.data.url;
-    const response = await ascii2d.search(imageUrl!);
-    return template("ascii2d", {
-      results: response.slice(0, 3),
-      $mustache: true,
-    });
-  });
-  ctx.command("ehentai").action(async ({ session }) => {
-    await session?.send("来张图");
-    const imageMessage = await session?.prompt(30 * 1000);
-    const imageUrl = segment
-      .parse(imageMessage!)
-      .find((value) => value.type === "image")?.data.url;
-    const response = await ehentai.search(imageUrl!);
-    return template("ehentai", {
-      results: response.slice(0, 3),
-      $mustache: true,
-    });
-  });
-  ctx.command("iqdb").action(async ({ session }) => {
-    await session?.send("来张图");
-    const imageMessage = await session?.prompt(30 * 1000);
-    const imageUrl = segment
-      .parse(imageMessage!)
-      .find((value) => value.type === "image")?.data.url;
-    const response = await iqdb.search(imageUrl!);
-    return template("iqdb", {
-      results: response.slice(0, 3),
-      $mustache: true,
-    });
-  });
-  ctx.command("saucenao").action(async ({ session }) => {
-    await session?.send("来张图");
-    const imageMessage = await session?.prompt(30 * 1000);
-    const imageUrl = segment
-      .parse(imageMessage!)
-      .find((value) => value.type === "image")?.data.url;
-    const response = await saucenao.search(imageUrl!);
-    return template("saucenao", {
-      results: response.slice(0, 3),
-      $mustache: true,
-    });
-  });
-  ctx.command("tracemoe").action(async ({ session }) => {
-    await session?.send("来张图");
-    const imageMessage = await session?.prompt(30 * 1000);
-    const imageUrl = segment
-      .parse(imageMessage!)
-      .find((value) => value.type === "image")?.data.url;
-    const response = await tracemoe.search(imageUrl!);
-    return template("tracemoe", {
-      results: response.slice(0, 3),
-      $mustache: true,
-    });
-  });
+  ctx.plugin(network).plugin(combine);
 }
--- src/plugin-image-search/utils.ts
+++ src/plugin-image-search/utils.ts
@@ -1,3 +1,4 @@
+import { segment } from "koishi";
 import { request } from "./network";
 
 export async function downloadImage(
@@ -14,4 +15,42 @@ export async function downloadImage(
     filename: filename ?? "image.jpg",
   };
   return { buffer, info };
+}
+
+export function firstImage(content: string): string | undefined {
+  return segment
+    .parse(content)
+    .find(({ type, data: { url } }) => type === "image" && url?.trim().length)
+    ?.data.url;
+}
+
+export function MultipleEnumerateType<T>(
+  enumerate: T,
+  splitter: string | RegExp = /\D+?/,
+): (source: string) => (keyof T)[] {
+  const keys = Object.keys(enumerate).filter(
+      (v) => typeof v === "string" && typeof enumerate[v] === "number",
+    ) as (keyof T)[],
+    values = Object.values(enumerate).filter(
+      (v) => typeof v === "number" && typeof enumerate[v] === "string",
+    ) as T[];
+  const valueMap = Object.fromEntries(values.map((v, i) => [v, keys[i]]));
+
+  if (keys.length !== values.length || keys.length <= 0)
+    throw new Error("EnumerateType: invalid enumerate");
+
+  return (source: string): (keyof T)[] => {
+    const selected = source
+      .split(splitter)
+      .map((v) => v.trim())
+      .filter((v) => v.length)
+      .filter((value) => typeof valueMap[+value] === "string");
+    if (selected.length <= 0)
+      throw new Error(
+        `未找到有效的枚举值, 可用值为:\n` +
+          values.map((value) => `${value}->${valueMap[value]}`).join(`\n`) +
+          `\n支持多个值, 用${splitter}分割`,
+      );
+    return selected.map((value) => valueMap[value]);
+  };
 }
